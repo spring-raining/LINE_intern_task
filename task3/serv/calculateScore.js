2fc48e11f2b0536058efeb2ff22318b1d1a8242a
spring-raining <fdonthavei@gmail.com>
2016-06-20 03:25:25 +0900

    Implive calculating score

--- task3/serv/calculateScore.js
+++ task3/serv/calculateScore.js
@@ -40,7 +40,27 @@ module.exports = function(statuses, dbClient, rootKey) {
       if (allCount === 0) {
         reject();
       }
-      resolve(allWeight / allCount);
+
+      resolve(cdf(((allWeight / allCount) - 0.5) * 8));
     }.bind(this));
   })
 };
+
+// CDF of the normal distribution
+// http://qiita.com/gigamori/items/e17e6f9faffb78822c56
+function cdf(x) {
+
+  // constants
+  var p  =  0.2316419;
+  var b1 =  0.31938153;
+  var b2 = -0.356563782;
+  var b3 =  1.781477937;
+  var b4 = -1.821255978;
+  var b5 =  1.330274429;
+
+  var t = 1 / (1 + p * Math.abs(x));
+  var Z = Math.exp(-x * x / 2) / Math.sqrt(2 * Math.PI);
+  var y = 1 - Z * ((((b5 * t + b4) * t + b3) * t + b2) * t + b1) * t;
+
+  return (x > 0) ? y : 1 - y;
+}
