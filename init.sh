2d769a396cc2d5865e4846f9c5fc748fef093992
EC2 Default User <ec2-user@ip-172-31-28-130.us-west-2.compute.internal>
2016-06-19 16:03:11 +0000

    Fix git submodule

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-git submodule update --init
+git submodule update --depth 1 --init
 cp mecab-ipadic-neologd/seed/mecab-user-dict-seed* neologd-seed/mecab-user-dict-seed.csv.xz
 unxz neologd-seed/mecab-user-dict-seed.csv.xz
 
